96612097f9b170ff53be4428ad97dfee80ce2fed
sahil-santosh-Indraksha <92012635+sahil-santosh-Indraksha@users.noreply.github.com>
2021-11-24 11:42:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var Recognition=new SpeechRecognition();
+function start(){
+    document.getElementById("textbox").innerHTML="";
+Recognition.start();
+}
+Recognition.onresult=function run(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML=content;
+}
