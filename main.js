31c31ebab92f904a0531c5c7c2f0b65b651d9c75
sahil-santosh-Indraksha <92012635+sahil-santosh-Indraksha@users.noreply.github.com>
2021-11-27 11:36:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ Recognition.onresult=function run(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
+speak();
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+Webcam.attach(camera);
+}
+Webcam.set({
+width:360,
+height:250,
+Image_format:'jpeg',
+jpeg_quality:90
+});
+camera=document.getElementById(camera);
